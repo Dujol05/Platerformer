96b699058e578082dadcd9d7ead07edf832da420
PARKDOHYUN7913 <dujol2258@gmail.com>
2024-05-11 17:46:14 +0900

    (2024-05-11) 플레이어 애니메이션

--- Platerformer Project/Assets/Scripts/Player Contoller.cs
+++ Platerformer Project/Assets/Scripts/Player Contoller.cs
@@ -19,12 +19,20 @@ public class PlayerContoller : MonoBehaviour
     private float moveInput;  // 플레이어의 방향 및 인폿 데이터 저장
 
     public Transform startTransform; // 캐릭터가 시작할 위치를 저장하는 변수
-    public Rigidbody2D rigidbody2D;  // 물리(강제) 기능을 제어하는 컴포넌트
+    public new Rigidbody2D rigidbody2D;  // 물리(강제) 기능을 제어하는 컴포넌트
 
     [Header("점프")]
     public bool isGrounded;          // true : 캐릭터가 점프 할 수 있는 상태, false : 점프 못함   
     public float groundDistance = 2f;
-    public LayerMask groundDistanc;
+    public LayerMask groundLayer;
+
+    [Header("Flip")]
+    public SpriteRenderer spriteRenderer;
+    private bool facingRight = true;
+    private int facingDirection = 1;
+
+    public Animator animator;
+    private bool isMove;
 
     void Start()
     {
@@ -35,26 +43,110 @@ public class PlayerContoller : MonoBehaviour
         //transform.position = new Vector2(transform.position.x, 10);
 
         // 현재 내 위치를 startTransform으로 변경
-        transform.position = startTransform.position;
+        InitializePlayerStatus();
 
+    }
+
+    void InitializePlayerStatus()
+    {
+        transform.position = startTransform.position;
     }
 
     // Update is called once per frame
     // 1 프레임에 한번 호출된다. - 반복적으로 실행
     void Update()
     {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 2, groundDistanc);
+        HandleAnimation();
+        CollisionCheck();
+        HandleInput();
+        HandleFlip();
+        Move();
+        FallDownCheck();
+        // 점프를 할 때 땅인지 아닌지 체크 하는지 기능 -> Collider Check
+        // 플레이어의 입력 값을 받아와야 한다.  a,d 키보드 좌 우 키를 눌렀을 때 -1 ~ 1 반환하는 클래스
+        // 플레이어의 입력을 받아오는 코드
 
-        // 플레이어의 입력 값을 받아와야 한다.
 
+    }
+
+    private void FallDownCheck()
+    {
+        // y의 높이가 특정 지점보다 낮을 때 낙사한 것으로 간주한다. -> 충돌 체크 대체
+        if(transform.position.y < -11)
+        {
+            InitializePlayerStatus();
+            rigidbody2D.velocity = Vector2.zero;
+            facingRight = true;
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private void HandleAnimation()
+    {
+        // rigidbody.velocity : 현재 rigidbody 속도 = 0 움직이지 않는 상태, !=0 움직이고 있는 상태
+        isMove = rigidbody2D.velocity.x != 0;
+        animator.SetBool("isMove", isMove);
+        animator.SetBool("isGrounded", isGrounded);
+        // SetFloat 함수에 의해서 y최대일 때 1로 변환.. y
+    }
+
+    /// <summary>
+    /// 플레이어 이동에 필요한 Bool 값을 제어하는 함수
+    /// </summary>
+    private void CollisionCheck()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer);
+    }
+
+    /// <summary>
+    /// 유저의 입력을 관리하는 함수
+    /// </summary>
+
+    private void HandleInput()
+    {
         moveInput = Input.GetAxis("Horizontal");
-        rigidbody2D.velocity = new Vector2(moveSpeed * moveInput, rigidbody2D.velocity.y);
 
+        JumpButton();
+    }
+
+    private void HandleFlip()
+    {
+        // 오른쪽 방향으로 바라보고 있을 때
+        if (facingRight && moveInput < 0)
+        {
+            Flip();
+        }
+        // 왼쪽 방향으로 바라보고 있을 때
+        else if(!facingRight && moveInput > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection = facingDirection * -1;
+        facingRight = !facingRight;
+        spriteRenderer.flipX = !facingRight;
+    }
+
+    private void JumpButton()
+    {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
-            // 점프 : Y Position _ rigidbody Y velocity를 점프 파워만큼 올려주면된다.
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);  
+            Jump();
         }
+    }
+
+
+    private void Move()
+    {
+        rigidbody2D.velocity = new Vector2(moveSpeed * moveInput, rigidbody2D.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
     }
 
     private void OnDrawGizmos()
---
+++ Platerformer Project/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    // 백터의 연산으로 구현
+
+    Vector3 offset;
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // transform = 카메라, 백터의 합, 빼기 -> A - B : B에서 출발해서 A까지 이동하는 화살표
+        offset = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
